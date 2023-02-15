33570e0e7adda89144f8c6edae343d90dccfbb1f
pruthvi72 <121791734+pruthvi72@users.noreply.github.com>
2023-02-15 17:26:37 +0530

    Update rating.py

--- assignments/hadoop/mr/rating.py
+++ assignments/hadoop/mr/rating.py
@@ -1,23 +1,16 @@
 from mrjob.job import MRJob
 
+class NumOfRatings(MRJob):
 
-class Rating_count(MRJob):
-
-        """ The below mapper() function defines the mapper for MapReduce and takes
-        key value argument and generates the output in tuple format .
-        The mapper below is splitting the line and generating a word with its own
-        count i.e. 1 """
+        # Mapper Function
         def mapper(self, _, line):
         	#print(line.split('\t'))
                 (userID,movieID, rating, timestamp) = line.split(',')
                 yield(rating, 1)
                         
-        """ The below reducer() is aggregating the result according to their key and
-        producing the output in a key-value format with its total count"""
+        # Reducer Function
         def reducer(self, rate, counts):
                 yield(rate, sum(counts))
 
-"""the below 2 lines are ensuring the execution of mrjob, the program will not
-execute without them"""
 if __name__ == '__main__':
-	Rating_count.run()
+	NumOfRatings.run()
